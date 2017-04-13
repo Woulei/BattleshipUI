b0be24b3a4eb4e2e21cb383676cc7e214f5f6275
ml7757 <matthew.w.laporte@gmail.com>
2017-04-13 15:15:47 +0200

    clean up GamePage

--- src/containers/GamePage.js
+++ src/containers/GamePage.js
@@ -1,16 +1,4 @@
 import React, { PureComponent } from 'react'
 
 export class GamePage extends PureComponent {
-
-  componentDidMount() {
-    this.props.fetchGames(gameId)
-  }
-
-  render() {
-    const { title } = this.props
-
-    return(
-      <div>{ title }</div>
-    )
-  }
 }
